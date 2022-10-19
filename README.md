ef477ca97facdd640b66fbd9ac92e814e29e9ecb
nathanielmuci <nate.muci@gmail.com>
2022-10-18 20:50:55 -0500

    Added readme, js and updated html

---
+++ README.md
@@ -0,0 +1,11 @@
+# 05Challenge : Work Schedular
+For this webpage I applied javascript, and jQuery to create a dynamic webpage that changes color according to the time of day as well as saving objects in the local storage and adding them back to the webpage after the page reloads.
+
+## Link
+Git repo:
+
+Deployed Git hub page:
+
+## Screenshot
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,45 @@
+// formats and gets time to dispaly in header and pulls time to check for background colors
 var rightNow = moment().format('LLLL');
-var timeCheck = moment().format('k')
+var timeCheck = moment().format('k');
 
+// creates selectors for buttons and time display
+var saveBtn = $(".saveBtn");
 var timeDisplayEl = $("#currentDay");
 var clearBtn = $("#clearBtn");
 
-var nineAm = $("#09:00")
-var tenAm = $("#10:00")
-var elevenAm = $("#11:00")
-var twelvePm = $("#12:00")
-var onePm = $("#13:00")
-var twoPm = $("#14:00")
-var threePm = $("#15:00")
-var fourPm = $("#16:00")
-var fivePm = $("#17:00")
-var sixPm = $("#18:00")
-var sevenPm = $("#19:00")
+// creates selectors for use with local storage and bgColor
+var nineAm = $("#09:00");
+var tenAm = $("#10:00");
+var elevenAm = $("#11:00");
+var twelvePm = $("#12:00");
+var onePm = $("#13:00");
+var twoPm = $("#14:00");
+var threePm = $("#15:00");
+var fourPm = $("#16:00");
+var fivePm = $("#17:00");
+var sixPm = $("#18:00");
+var sevenPm = $("#19:00");
 
-var count = true;
-
+// adds current time to header
 function displayTime() {
-    timeDisplayEl.text(rightNow);
-  }
-  setInterval(displayTime, 1000);
+  timeDisplayEl.text(rightNow);
+}
+setInterval(displayTime, 1000);
 
- 
-  
- 
-// if (timeCheck > )
-   // backgroundColor:"grey"
-// else (timeCheck < )
-    // backgroundColor:"Green"
-// else(timeCheck ==)
-    // backgroundColor:"red"
-function bGColor(){ 
+$(document).ready(function(){
+  loadSave();
+  bgColor();
+});
+// loads previous saves and adds them to the webpage
+function loadSave() {
+  var load9 = JSON.parse(localStorage.getItem("09:00am"));
+  nineAm.val(load9)
+
+}
+
+// changes the back ground color by cycling through int from "#id" and comparing
+// to current time, then adding class acordingly
+function bgColor(){ 
   $("textarea").each(function(){
     var idCheck = parseInt($(this).attr("id"));
     timeCheck = parseInt(timeCheck);
@@ -44,5 +51,18 @@ function bGColor(){
       $(this).addClass("present");
     }
   })
+
 }
-bGColor();
+saveBtn.on("click", function(){
+  userInput = $(this).siblings("textarea").val();
+  saveTime = $(this).siblings(".time-block").text();
+  localStorage.setItem(saveTime, JSON.stringify(userInput));
+});
+
+clearBtn.on("click",function(){
+  localStorage.clear();
+});
+
+// runs function after DOM is ready to execute javascript
+  
+console.log(nineAm);
